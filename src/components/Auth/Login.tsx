d3aaf80e57eb690c16e95fd639fa8ee2de18ed00
Chirayu Maru <chirayumaru@gmail.com>
2025-08-01 15:29:42 +0530

    Update login

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -139,7 +139,7 @@ const Login: React.FC = () => {
         <div className="rounded-xl border-[3px] border-gray-700 shadow-2xl w-full max-w-none scale-[1.10] md:ml-14">
           <a href="/"> {/* Replace with your actual route */}
             <img
-              src="http://res.cloudinary.com/dkoiyuyhj/image/upload/v1754042014/bbzm60eqwh86j7yhd49y.png"
+              src="https://res.cloudinary.com/dkoiyuyhj/image/upload/v1754042014/bbzm60eqwh86j7yhd49y.png"
               alt="Dashboard Preview"
               className="object-contain w-full h-full rounded-xl"
             />
